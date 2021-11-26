44929c5cb7851b23544fa63136a6d91235083a11
AlexsandroD <adrdra88@gmail.com>
2021-11-26 12:14:39 -0800

    bonus 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const app = new Vue({
-    el:'#root',
-    data:{
-        images:
-        [
+    el: '#root',
+    data: {
+        
+        images: [
             './img/01.jpg',
             './img/02.jpg',
             './img/03.jpg',
@@ -10,7 +10,7 @@ const app = new Vue({
             './img/05.jpg',
         ],
 
-        title:[
+        title: [
             'Svezia',
             'Svizzera',
             'Gran Bretagna',
@@ -18,7 +18,7 @@ const app = new Vue({
             'Paradise'
         ],
 
-        description:[
+        description: [
             'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
             'Lorem ipsum',
             'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
@@ -26,28 +26,67 @@ const app = new Vue({
             'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
         ],
 
-        
-        counter: 0
-  
+
+        counter: 0,
+        timer: 0,
+
 
     },
-    methods:{
-        prev: function(){
-            if(this.counter == 0){
-                this.counter = this.images.length -1;
-            }else{
-
+    methods: {
+        prev: function () {
+            if (this.counter == 0) {
+                this.counter = this.images.length - 1;
+                this.resetPlay();
+            } else {
                 this.counter--;
             }
         },
 
-        next: function(){
-            if(this.counter == this.images.length -1){
+        next: function () {
+            if (this.counter == this.images.length - 1) {
                 this.counter = 0;
-            }else{
+                this.resetPlay();
 
+            } else {
                 this.counter++
             }
+        },
+
+        selectImg: function (index) {
+            this.counter = index;
+            this.resetPlay();
+        },
+
+        play: function () {
+            let app = this;
+            this.timer = setInterval(function () {
+                app.next();
+            }, 2000);
+        },
+        resetPlay: function () {
+            clearInterval(this.timer);
+            this.play();
+        },
+        
+        created: function () {
+            this.play();
         }
     }
-});
+        
+        
+        
+        
+        
+        // ---aggiungi classe active utilizzando una funzione----
+        
+        // setActiveClass: function (index){
+            //     if(index == this.counter){
+                //         return 'active-t';
+                //     }else{
+                    //         return '';
+                    //     }
+                    // }
+                    
+                    
+    });
+                
